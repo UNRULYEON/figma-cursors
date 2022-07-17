e52214f615d3ee2831f8bfb6a781a7b42680919c
Amar Kisoensingh <amar@kisoensingh.sh>
2022-07-17 19:43:15 +0200

    Updates manifest and package.json

--- code.ts
+++ code.ts
@@ -21,7 +21,7 @@ if (figma.editorType === 'figma') {
       for (let i = 0; i < msg.count; i++) {
         const rect = figma.createRectangle();
         rect.x = i * 150;
-        rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
         figma.currentPage.appendChild(rect);
         nodes.push(rect);
       }
@@ -34,7 +34,7 @@ if (figma.editorType === 'figma') {
     figma.closePlugin();
   };
 
-// If the plugins isn't run in Figma, run this code
+  // If the plugins isn't run in Figma, run this code
 } else {
   // This plugin will open a window to prompt the user to enter a number, and
   // it will then create that many shapes and connectors on the screen.
@@ -56,7 +56,7 @@ if (figma.editorType === 'figma') {
         // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
         shape.shapeType = 'ROUNDED_RECTANGLE'
         shape.x = i * (shape.width + 200);
-        shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+        shape.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
         figma.currentPage.appendChild(shape);
         nodes.push(shape);
       };
@@ -71,7 +71,7 @@ if (figma.editorType === 'figma') {
         };
 
         connector.connectorEnd = {
-          endpointNodeId: nodes[i+1].id,
+          endpointNodeId: nodes[i + 1].id,
           magnet: 'AUTO',
         };
       };
